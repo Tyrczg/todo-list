5e6cb6f69069793b74d324d7d04c94c289c1bae9
Tyrczg <tyrczg@wp.pl>
2020-05-05 23:24:58 +0200

    dodano funkcje dodawania zadan

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const Koa = require('koa')
 const serve = require('koa-static')
 const logger = require('koa-logger')
+const parse = require('koa-bodyparser')
 
 const app = new Koa()
 app.use(logger())
@@ -11,7 +12,7 @@ app.use(serve('client'))
 
 const userRoutes = require('./routes/users')
 app.use(userRoutes.routes())
-
+app.use(parse())
 const taskRoutes = require('./routes/tasks')
 app.use(taskRoutes.routes())
 
